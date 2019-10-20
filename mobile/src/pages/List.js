b4b92467eaf66017928cdd911a29ed25b86ca27b
Erick Costa <erick.costa.ti@gmail.com>
2019-10-20 17:55:52 -0300

    Content layout

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,12 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, AsyncStorage} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet, AsyncStorage } from 'react-native';
 
-export default function List({navigation}){
+import logo from '../assets/logo.png';
+
+export default function List({ navigation }) {
     const [techs, setTechs] = useState([]);
 
     useEffect(() => {
         AsyncStorage.getItem('techs').then(storageTechs => {
-            if(storageTechs){
+            if (storageTechs) {
                 const techsArray = storageTechs.split(',').map(tech => tech.trim());
                 console.log('Techs', techsArray)
                 setTechs(techsArray);
@@ -14,38 +16,66 @@ export default function List({navigation}){
         })
     }, []);
 
-    async function logout(){
+    async function logout() {
         await AsyncStorage.removeItem('user');
         await AsyncStorage.removeItem('techs');
 
         navigation.navigate('Login');
     }
-    
+
     return (
-        <View>
-            <TouchableOpacity onPress={logout} style={styles.button}>
-                <Text style={styles.buttonText}>Sair</Text>
-            </TouchableOpacity>
-            <Text>{techs}</Text> 
-        </View>
+        <View style={styles.container}>
+            <View style={styles.top}>
+                <Image style={styles.logo} source={logo} />
+            </View>
+
+            <View style={styles.content}>
+                <Text>Conteúdo</Text>
+            </View>
+
+            <View style={styles.bottom}>
+                <TouchableOpacity onPress={logout} style={styles.button}>
+                    <Text style={styles.buttonText}>Sair</Text>
+                </TouchableOpacity>
+            </View>
+        </View >
     )
 }
 
 const styles = StyleSheet.create({
-    
-    button: {
+
+    container: {
+        flex: 1,
+    },
+
+    top: {
+        flex: 1,
         marginTop: 25,
-        marginHorizontal: 2,
-        height: 42,
-        width: 50,
-        backgroundColor: '#f05a5b',
+        paddingTop: 5,
+        alignItems: "center",
+        minHeight: 17
+    },
+
+    logo: {
+
+    },
+
+    content: {
+        flex: 12,
+    },
+
+    bottom: {
+        flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 2,
+        alignItems: 'flex-end',
+    },
+
+    button: {
+        marginRight: 10,
     },
 
     buttonText: {
-        color: '#ffffff'
+        color: '#000000'
     }
 
 });
